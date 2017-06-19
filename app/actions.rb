896ab22da702192171876484cf7f74d22e67ca15
Ana Hall <anahall424@gmail.com>
2017-06-19 22:38:17 +0000

    New Filed Added

--- app/actions.rb
+++ app/actions.rb
@@ -1,60 +1,14 @@
-def humanized_time_ago(time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes / 60} hours ago"
-    else
-        "#{time_ago_in_minutes} minutes ago"
-    end
-end
-
-get '/but' do
-  erb :but
-end
-
-
-get '/posts/:id' do
-  "Hello #{params["id"]}"
-end
-
 get '/' do
-    #@posts = Post.all
-    
-    @posts = [{
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/shark.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-           username: "sharky_j",
-           text: "Out for thr long weekend... too embarrassed to show ya'll the beach bod!"
-        }]
-    },{
-        username: "kirk_walum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk_walum",
-            text: "#weekendvibes"
-        }]
-    },{
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin_peppa",
-            text: "lunchtime! ;)" 
-        }, {
-            username: "loveless_cuttlefish95",
-            text: "Your fin is tiny! Dweeb! rofl (rolling on my fin laughing)"
-        }]
-    }]
-    
-    erb :index
+   @posts = Post.order(created_at: :desc) 
+    erb(:index)
+end
+
+get '/signup' do    # if a user navigates to the path "/signup",
+    erb(:signup)    # render "app/views/signup.erb"
+end
+
+post '/signup' do
+    @user = User.new(params)
+    @user.save
+    erb(:welcome)
 end
