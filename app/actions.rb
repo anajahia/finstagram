37b26c2214d30c389f148ff5c8ad1514884cc8f2
Ana Hall <anahall424@gmail.com>
2017-09-15 02:57:11 +0000

    asdf

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,68 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+require "sinatra"
+require "haml"
+
 get '/' do
    @posts = Post.order(created_at: :desc) 
     erb(:index)
 end
 
-get '/signup' do    # if a user navigates to the path "/signup",
-    erb(:signup)    # render "app/views/signup.erb"
+
+get '/signup' do
+    @user = User.new
+    erb(:signup)
+end
+
+
+get '/users' do
+    @users = User.all
+    erb(:users)
 end
 
 post '/signup' do
     @user = User.new(params)
-    @user.save
-    erb(:welcome)
+    
+    # if user validations pass and user is saved
+    if @user.save
+        redirect to('/login')
+    else
+        erb(:signup)
+    end
 end
+
+get '/login' do
+    erb(:login)
+end
+ 
+ 
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to ('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end
+
+get '/admin' do
+    haml(:admin)
+end
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
-class User <ActiveRecord::Base
+class User < ActiveRecord::Base
+ 
+ validates_presence_of :email, :avatar_url, :username, :password
+ validates_uniqueness_of :email, :username
 
-has_many :posts
-has_many :comments
-has_many :likes
+ has_many :posts
+ has_many :comments
+ has_many :likes
 
 end
