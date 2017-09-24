580c86e4066ce05934dde13648d1ccfd9ecb3204
Ana Hall <anahall424@gmail.com>
2017-09-24 18:28:43 +0000

    asdf

--- app/actions.rb
+++ app/actions.rb
@@ -63,6 +63,23 @@ get '/admin' do
     haml(:admin)
 end
 
+get '/posts/new' do
+    erb(:"posts/new")
+end
+
+post '/posts' do
+    photo_url = params[:photo_url]
+    
+    @post = Post.new({ photo_url: photo_url, user_id: current_user.id })
+    
+    if @post.save  
+        redirect(to('/'))
+    else
+        @post.errors.full_messgae.inspect
+    end
+end
+
+
 
 
 
