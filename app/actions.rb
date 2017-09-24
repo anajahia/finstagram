46de467911ef4fae20f9159eb5c2d17dfde64e0d
Ana  J Hall <anajhall@Tests-MacBook-Air.local>
2017-09-24 16:18:27 -0400

    Move those login routes out

--- app/actions.rb
+++ app/actions.rb
@@ -21,56 +21,6 @@ get '/' do
 end
 
 
-get '/signup' do
-    @user = User.new
-    erb(:signup)
-end
-
-
-get '/users' do
-    @users = User.all
-    erb(:users)
-end
-
-post '/signup' do
-  begin
-    @user = User.new(params.slice(:email, :avatar_url, :username, :password))
-    
-    # if user validations pass and user is saved
-    if @user.save
-        redirect to('/login')
-    else
-        erb(:signup)
-    end
-  rescue
-    binding.pry
-  end
-end
-
-get '/login' do
-    erb(:login)
-end
- 
- 
-post '/login' do
-    username = params[:username]
-    password = params[:password]
-    
-    user = User.find_by(username: username)
-    
-    if user && user.password == password
-        session[:user_id] = user.id
-        redirect to ('/')
-    else
-        @error_message = "Login failed."
-        erb(:login)
-    end
-end
-
-get '/logout' do
-    session[:user_id] = nil
-    redirect to('/')
-end
 
 
 get '/posts/new' do
